91a4ec56d2c2fe3fcd3d237f3ae3267060b3d6d3
Blubmin <imeeder@cox.net>
2019-08-15 16:36:45 -0700

    Add eigen typedefs for ceres simplicity.

--- pixel_engine/eigen_typedefs.h
+++ pixel_engine/eigen_typedefs.h
@@ -1,5 +1,17 @@
 #include <Eigen/Geometry>
 
 namespace Eigen {
-  typedef AlignedBox<float, 2> Rectf;
-}
+typedef AlignedBox<float, 2> Rectf;
+
+template <typename T>
+using Vector2 = Matrix<T, 2, 1>;
+
+template <typename T>
+using Vector3 = Matrix<T, 3, 1>;
+
+template <typename T>
+using Vector4 = Matrix<T, 4, 1>;
+
+template <typename T>
+using Isometry3 = Transform<T, 3, Isometry>;
+}  // namespace Eigen
--- pixel_engine/shaders/post.vert
+++ pixel_engine/shaders/post.vert
@@ -10,17 +10,6 @@ void main() {
 	vec2 pos = a_pos;
 	pos.x = clamp(pos.x, roi.x, roi.z);
 	pos.y = clamp(pos.y, roi.y, roi.w);
-//	if (a_pos.x < 0) {
-//		pos.x = (roi.x * 2) - 1;
-//	} else {
-//		pos.x = (roi.z * 2) - 1;
-//	}
-//
-//	if (a_pos.y < 0) {
-//		pos.y = (roi.y * 2) - 1;
-//	} else {
-//		pos.y = (roi.w * 2) - 1;
-//	}
 	gl_Position = vec4(pos, 0, 1);
 	v_uv = (a_pos + vec2(1, 1)) / 2;
 }
